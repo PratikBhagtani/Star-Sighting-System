893882b67842222e056c43d0219a85fc842760a4
pRaTiK BhAgTaNi <pRaTiK BhAgTaNi@172.17.180.76>
2016-11-06 12:18:51 -0600

    aNGLE cHANGES

--- SoftwareProcess/Navigation/prod/Angle.py
+++ SoftwareProcess/Navigation/prod/Angle.py
@@ -2,7 +2,7 @@ class Angle():
     
     def __init__(self):
         #self.angle = ...       set to 0 degrees 0 minutes
-        self.angle = '0d0'
+        self.angle = 0
            
     def setDegrees(self,degrees = 0.0):
                 if(isinstance(degrees, float)):
@@ -18,7 +18,10 @@ class Angle():
                     
               
                 self.angle = degrees % 360
+                self.angle = self.angle * 60
                 self.angle = abs(self.angle)
+                self.angle = round(self.angle, 1)
+                self.angle = self.angle/60
                 return self.angle
 
     
@@ -30,19 +33,23 @@ class Angle():
             
             elif (not(isinstance(angleString, str))):
                 raise ValueError(fname + "Degree not Entered")
-            
+             
             elif angleString.find("d") == False:
                 raise ValueError(fname + "Degree not Entered")
             
             else:
-                print "executed"
-                degreenminute = angleString.split("d")
-                self.degree = (degreenminute[0])
-                self.minute = float(degreenminute[1])
                 
-                if self.degree == int(self.degree):
-                    raise ValueError(fname + "Degree not Entered")
-                self.degree = int(self.degree)
+                degreenminute = angleString.split("d")
+                self.degree = int(degreenminute[0])
+                self.minute = float(degreenminute[1])
+                if self.minute < 0:
+                    raise ValueError("Angle.setDegreesAndMinutes:    Minutes cannot be negative.")
+                
+                countdec = degreenminute[1].find(".")
+                if len(degreenminute[1]) - countdec == 2 :
+                    pass
+                else:
+                    raise ValueError("Angle.setDegreesandMinutes:    Minutes can contain only one decimal places.")
                 
                 if self.minute != round(self.minute,1):
                     raise ValueError('Angle.setDegreesAndMinutes:')
@@ -56,115 +63,53 @@ class Angle():
                     self.angle += 360
             return self.angle  
             
-        except AssertionError as e:
-            print (e)
+        except:
+            raise ValueError("Angle.setDegreesAndMinutes:   Enter Data not valid")
 
             
-    def add(self, angle):
-         
-        try:
-            degreenminute = self.angle.split("d")
-            self.degree = int(degreenminute[0])
-            self.minute = float(degreenminute[1])
-             
-            if self.minute > 0:
-                self.degree = int(self.degree)
-                self.degree = self.degree % 360 
-                self.minute = float(self.minute) 
-                self.minute = round(self.minute,1)
-                angle = self.degree+'d'+self.minute
-                self.Angle = Angle + angle
-                degreenminute = self.Angle.split("d")
-                self.degree = int(degreenminute[0])
-                self.minute = float(degreenminute[1])
-                self.degree = self.degree % 360
-                if self.minute == 30:
-                    self.angle = self.degree + 0.5
-                elif self.minute == 60:
-                    self.angle = self.degree + 1
-                else:
-                    self.angle = self.degree
-                return self.angle
-             
-             
-            else:
-                print "fail"
-             
-        except ValueError as b:
-            print (b), '"Enter Degree in integer Value and Minutes in Integer or Float values."'
+    def add(self, angle = None):
+
+
+        if not(isinstance(angle, Angle)):
+            raise ValueError("Angle.add:")
+        else:
+            self.angle += angle.getDegrees()
+            self.angle %= 360
+            return self.angle
+            
+    def subtract(self, angle = None):
  
+        if not(isinstance(angle, Angle)):
+            raise ValueError("Angle.subtract:")
+        else:
+            self.angle -= angle.angle
+            self.angle %= 360
+            return self.angle
      
-    def subtract(self, angle):
- 
-        try:
-            degreenminute = angle.split("d")
+    def compare(self, angle = None):
+        if not(isinstance(angle, Angle)):
+            raise ValueError("Angle.compare: Not instance of Angle")
+        if self.angle == angle.getDegrees():
+            return 0
+        elif self.angle > angle.getDegrees():
+            return 1
+        else:
+            return -1
+       
              
-             
-            if minute > 0:
-                degree = int(degree) 
-                degree = degree % 360
-                minute = float(minute) 
-                minute = round(minute,1)
-                angle = degree+'d'+minute
-                self.Angle = Angle - angle
-                degree,minute = self.Angle.split("d")
-                degree = degree % 360
-                if minute == 30:
-                    self.Angle = degree + 0.5
-                elif minute == 60:
-                    self.Angle = degree + 1
-                else:
-                    self.Angle = degree
-                return self.Angle
-            else:
-                print "fail"
-             
-        except ValueError as b:
-            print (b), '"Enter Degree in integer Value and Minutes in Integer or Float values."'
-     
-    def compare(self, angle):
-         
-            try:
-                degree,minute = angle.split("d")
-             
-             
-                if minute > 0:
-                    degree = int(degree)
-                    degree = degree % 360 
-                    minute = float(minute) 
-                    minute = round(minute,1)
-                    angle = degree+'d'+minute
-                     
-                 
-                else:
-                    print "fail"
-                if self.Angle > angle:
-                    return -1
-                elif self.Angle == angle:
-                    return 0
-                else:
-                    return 1
-             
-            except ValueError as b:
-                print (b), '"Enter Degree in integer Value and Minutes in Integer or Float values."'
-     
+            
     def getString(self):
          
-        degree,minute = self.angle.split("d")
-             
-             
-        if minute > 0:
-            degree = int(degree)
-            degree = degree % 360 
-            minute = float(minute) 
-            minute = round(minute,1)
-            minute = minute % 60
-            self.angle = degree+'d'+minute
-            return self.angle
+       degree = int(self.angle)
+       minute = self.angle - degree
+       minute *= 60
+       minute = round(minute,1)
+       degree = str(degree)
+       minute = str(minute)
+       return degree + 'd' + minute
                      
                  
-        else:
-            print "fail"
+        
      
     def getDegrees(self):
         
